1fafcabff78a45536089bfedd76579d4bbd5e18a
Mohit Reddy <mohitreddy1996@gmail.com>
2016-12-11 00:25:46 +0530

    Initial commit

---
+++ public/javascripts/app.js
@@ -0,0 +1,9 @@
+/**
+ * Created by mohit on 11/12/16.
+ */
+var app = angular.module('flapperNews', []);
+
+app.controller('MainCtrl', [
+'$scope', function($scope){
+    $scope.test = 'Real Time Group Chat Application';
+}]);
