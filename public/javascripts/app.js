c473859338add6b91e75591637de873bcd458431
Mohit Reddy <mohitreddy1996@gmail.com>
2016-12-17 22:17:22 +0530

    Changes 1

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,9 +1,26 @@
 /**
  * Created by mohit on 11/12/16.
  */
-var app = angular.module('flapperNews', []);
+var app = angular.module('LoginPage', []);
 
 app.controller('MainCtrl', [
-'$scope', function($scope){
-    $scope.test = 'Real Time Group Chat Application';
-}]);
+'$scope',
+    function($scope){
+        $scope.test = 'Real Time Group Chat Application';
+    },
+    $scope.confirmLogin = function(){
+        var userId = $scope.userId;
+        var passwrd = $scope.userPasswrd;
+        var loginData = {
+            userId: userId,
+            passwrd: passwrd
+        };
+
+        // send the data.
+        $http.post('/login', data).success(function (data, status) {
+            console.log("Login Completed Successfully");
+        }).error(function (data, status) {
+            console.log("Login Failed!");
+        })
+    }
+]);
